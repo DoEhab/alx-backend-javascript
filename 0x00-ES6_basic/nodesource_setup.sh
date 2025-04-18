05c08f70d870e97c467b544d727c48bfdb342964
doehab <dohaehab@gmail.com>
2025-04-18 18:05:50 +0200

    task 0

---
+++ 0x00-ES6_basic/nodesource_setup.sh
@@ -0,0 +1,14 @@
+<html>
+<head><title>404 Not Found</title></head>
+<body>
+<h1>404 Not Found</h1>
+<ul>
+<li>Code: NoSuchKey</li>
+<li>Message: The specified key does not exist.</li>
+<li>Key: setup_12.11.1</li>
+<li>RequestId: EASPJTE1RKHTFKR0</li>
+<li>HostId: +MGhuyy/ZWwcq8VOcMB7hAmWbjvyQuzI5b28DwAecvgHPz5DBqZO7GVarjjgJmYDL8bLceltRdg=</li>
+</ul>
+<hr/>
+</body>
+</html>

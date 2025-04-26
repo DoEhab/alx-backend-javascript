1df101c8ec74f535f63cf21c8b60d90ee7b9e430
dohaehab <dohaehab@gmail.com>
2025-04-26 21:11:20 +0300

    task 6

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,6 @@
 export default function cleanSet(set, startString) {
   let result = '';
+  if (typeof startString !== 'string' || startString === '') return '';
   for (const value in set) if (value.startWith(startString)) result += (result ? '-' : '') + value.slice(startString.length);
   return result;
 }

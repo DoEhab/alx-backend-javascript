8c07e7dab070121b0a153be4195b29c01f69f603
dohaehab <dohaehab@gmail.com>
2025-04-26 20:01:10 +0300

    task 5

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,7 +1,6 @@
 export default function createInt8TypedArray(length, position, value) {
-    if (position >= length)
-        throw new RangeError("Position outside range");
-    const result = new DataView(new ArrayBuffer(length));
-    result.setInt8(position, value);
-    return result;
+  if (position >= length) throw new RangeError('Position outside range');
+  const result = new DataView(new ArrayBuffer(length));
+  result.setInt8(position, value);
+  return result;
 }

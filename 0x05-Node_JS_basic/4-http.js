08706b6f5149d4d182c1fee56c155d2a6bf11fc1
dohaehab <dohaehab@gmail.com>
2025-06-11 23:39:04 +0300

    task 4

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,11 +1,11 @@
+// create temp server
 const http = require('http');
 
 const app = http.createServer((req, res) => {
   const responseText = 'Hello ALX!';
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.setHeader('Content-Length', responseText.length);
-  res.write(Buffer.from(responseText));
+  res.end(responseText);
 });
 
 app.listen(1245);

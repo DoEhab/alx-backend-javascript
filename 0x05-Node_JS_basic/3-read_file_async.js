1999e3c31ea2d81c73bf575b2672b4bfb2eecf48
dohaehab <dohaehab@gmail.com>
2025-06-11 23:09:05 +0300

    TASK 3

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,49 +1,56 @@
 const fs = require('fs');
 
 function countStudents(path) {
-  fs.readFile(path, 'utf8', (err, fileData) => {
-    if (err) {
-      console.error('Cannot load the database');
-      return;
-    }
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, fileData) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
 
-    const lines = fileData.split('\n').filter((line) => line.trim() !== '');
-    if (lines.length <= 1) return;
+      const lines = fileData.split('\n').filter((line) => line.trim() !== '');
+      if (lines.length <= 1) {
+        resolve();
+        return;
+      }
 
-    const headers = lines[0].split(',');
-    const fieldsIndex = headers.indexOf('field');
-    const firstNameIndex = headers.indexOf('firstname');
+      const headers = lines[0].split(',');
+      const fieldsIndex = headers.indexOf('field');
+      const firstNameIndex = headers.indexOf('firstname');
 
-    const fieldsCount = {};
-    const studentData = {};
+      const fieldsCount = {};
+      const studentData = {};
 
-    for (let i = 1; i < lines.length; i += 1) {
-      const line = lines[i].trim();
-      if (line !== '') {
-        const fields = line.split(',');
-        const field = fields[fieldsIndex];
-        const studName = fields[firstNameIndex];
+      for (let i = 1; i < lines.length; i += 1) {
+        const line = lines[i].trim();
+        if (line !== '') {
+          const fields = line.split(',');
+          const field = fields[fieldsIndex];
+          const studName = fields[firstNameIndex];
 
-        if (!fieldsCount[field]) {
-          fieldsCount[field] = 0;
-          studentData[field] = [];
+          if (!fieldsCount[field]) {
+            fieldsCount[field] = 0;
+            studentData[field] = [];
+          }
+
+          fieldsCount[field] += 1;
+          studentData[field].push(studName);
         }
-
-        fieldsCount[field] += 1;
-        studentData[field].push(studName);
       }
-    }
 
-    const totalStudents = Object.values(fieldsCount).reduce((sum, val) => sum + val, 0);
-    console.log(`Number of students: ${totalStudents}`);
+      const totalStudents = Object.values(fieldsCount).reduce((sum, val) => sum + val, 0);
+      console.log(`Number of students: ${totalStudents}`);
 
-    for (const field in fieldsCount) {
-      if (Object.hasOwn(fieldsCount, field)) {
-        const count = fieldsCount[field];
-        const list = studentData[field].join(', ');
-        console.log(`Number of students in ${field}: ${count}. List: ${list}`);
+      for (const field in fieldsCount) {
+        if (Object.hasOwn(fieldsCount, field)) {
+          const count = fieldsCount[field];
+          const list = studentData[field].join(', ');
+          console.log(`Number of students in ${field}: ${count}. List: ${list}`);
+        }
       }
-    }
+
+      resolve();
+    });
   });
 }
 

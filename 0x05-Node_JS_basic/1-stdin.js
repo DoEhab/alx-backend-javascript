676b48d198c344c718e3d945edd2018adcebce7a
dohaehab <dohaehab@gmail.com>
2025-06-11 20:39:41 +0300

    TASK 0

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,3 +1,9 @@
-console.log('Welcome to ALX, what is your name?\n');
-const userName = process.argv[2];
-console.log(`Your name is: ${userName}\n`);
+const readInput = require('readline');
+
+readInput.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+}).question('Welcome to ALX, what is your name?\n', (name) => {
+  console.log(`Your name is: ${name}\n`);
+  process.exit();
+});

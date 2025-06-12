80f5908b654d2abf441f3a3d71633890df7daa12
dohaehab <dohaehab@gmail.com>
2025-06-13 01:38:37 +0300

    task 3

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -1,27 +1,29 @@
 const sinon = require('sinon');
-const utils = require('./utils');
-const sendPaymentRequestToApi = require('./3-payment');
+const sendPaymentRequestToApi = require('./5-payment');
+const Utils = require('./utils');
 
 describe('sendPaymentRequestToApi', () => {
   let consoleSpy;
+
   beforeEach(() => {
     consoleSpy = sinon.spy(console, 'log');
   });
+
   afterEach(() => {
     consoleSpy.restore();
   });
-  it('call Utils.calculateNumber', () => {
 
+  it('called with 100 and 20', () => {
     sendPaymentRequestToApi(100, 20);
 
-    sinon.assert.calledOnce(spy);
-    sinon.assert.calledOnceWithExactly(consoleSpy, 'SUM', 100, 20);
+    sinon.assert.calledOnce(consoleSpy);
+    sinon.assert.calledWithExactly(consoleSpy, 'The total is: 120');
   });
-  it('call Utils.calculateNumber', () => {
+
+  it('called with 10 and 10', () => {
     sendPaymentRequestToApi(10, 10);
 
-    sinon.assert.calledOnce(spy);
-    sinon.assert.calledOnceWithExactly(consoleSpy, 'SUM', 10, 10);
-
+    sinon.assert.calledOnce(consoleSpy);
+    sinon.assert.calledWithExactly(consoleSpy, 'The total is: 20');
   });
 });

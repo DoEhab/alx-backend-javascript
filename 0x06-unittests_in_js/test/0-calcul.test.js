19de02177b60e5fdbd09a66b2ebcc763539b4685
dohaehab <dohaehab@gmail.com>
2025-06-12 23:17:30 +0300

    task 0

---
+++ 0x06-unittests_in_js/test/0-calcul.test.js
@@ -0,0 +1,8 @@
+const assert = require('assert');
+const calcNumber = require('../0-calcul');
+
+describe('calculateNumber', () => { 
+    it('', ()=>{
+        assert.strictEqual(calcNumber(1.3, 1.7), 3);
+    });
+});

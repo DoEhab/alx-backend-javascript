17e592546e1701a5cc6ae2bfa831b56d59bf29c1
dohaehab <dohaehab@gmail.com>
2025-06-13 16:55:47 +0300

    task 3

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -4,29 +4,29 @@ const app = require('./api');
 const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 
-describe('Index page', () => {
-    it('return status code', (done) =>{
-        chai.request(app)
-        .get('/')
-        .end((err, res) => {
-            expect(res).to.have.status(200);
-            done();
-        });
-    });
-    it('return data', (done) =>{
-        chai.request(app)
-        .get('/')
-        .end((err, res) => {
-            expect(res.text).to.equal('Welcome to the payment system');
-            done();
-        });
-    });
-    it('return header', (done) =>{
-        chai.request(app)
-        .get('/')
-        .end((err, res) => {
-            expect(res).to.have.header('content-type', /text\/html/);
-            done();
-        });
-    });
-});
+// describe('Index page', () => {
+//     it('return status code', (done) =>{
+//         chai.request(app)
+//         .get('/')
+//         .end((err, res) => {
+//             expect(res).to.have.status(200);
+//             done();
+//         });
+//     });
+//     it('return data', (done) =>{
+//         chai.request(app)
+//         .get('/')
+//         .end((err, res) => {
+//             expect(res.text).to.equal('Welcome to the payment system');
+//             done();
+//         });
+//     });
+//     it('return header', (done) =>{
+//         chai.request(app)
+//         .get('/')
+//         .end((err, res) => {
+//             expect(res).to.have.header('content-type', /text\/html/);
+//             done();
+//         });
+//     });
+// });

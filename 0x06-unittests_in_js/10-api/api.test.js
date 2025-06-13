748d7d1caa90866fefebdd19786f90fbb9c774ac
dohaehab <dohaehab@gmail.com>
2025-06-13 19:41:42 +0300

    task 3

---
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -0,0 +1,63 @@
+const request = require('request');
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('Index Page', () => {
+  const API_URL = 'http://localhost:7865';
+
+    it('test responses', (done) => {
+        request.get(`${API_URL}/`, (err, res, body) => {
+        expect(res.statusCode).to.be.equal(200);
+        expect(body).to.be.equal('Welcome to the payment system');
+        done();
+        });
+    });
+
+  it('test responses', (done) => {
+    request.get(`${API_URL}/cart/8`, (err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Payment methods for cart 8');
+      done();
+    });
+  });
+  it('test responses', (done) => {
+    request.get(`${API_URL}/cart/-1`, (err, res) => {
+      expect(res.statusCode).to.be.equal(404);
+      done();
+    });
+  });
+  it('test responses', (done) => {
+    request.get(`${API_URL}/cart/xrir-rr`, (err, res) => {
+      expect(res.statusCode).to.be.equal(404);
+      done();
+    });
+  });
+
+  it('test available payments responses', (done) => {
+    request.get(`${API_URL}/available_payments`, (err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(JSON.parse(body)).to.deep.equal(
+        {
+          payment_methods: {
+            credit_cards: true,
+            paypal: false
+          }
+        }
+      );
+      done();
+    });
+  });
+
+  it('test login responses', (done) => {
+    const userName = 'Do';
+    request.post(
+      {
+        url: `${API_URL}/login`,
+        json: { userName },
+      }, (err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal(`Welcome ${userName}`);
+      done();
+    });
+  });
+});

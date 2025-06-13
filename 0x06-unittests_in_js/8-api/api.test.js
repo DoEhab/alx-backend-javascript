77d77bf79f3127598f2154e20db8bb65155b054e
dohaehab <dohaehab@gmail.com>
2025-06-13 17:20:34 +0300

    task 3

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -6,9 +6,8 @@ describe('Index Page', () => {
   const API_URL = 'http://localhost:7865';
 
   it('test responses', (done) => {
-    request.get(`${API_URL}/`, (_err, res, body) => {
+    request.get(`${API_URL}/`, (err, res, body) => {
       expect(res.statusCode).to.be.equal(200);
-      expect(res).to.have.header('content-type', /text\/html/);
       expect(body).to.be.equal('Welcome to the payment system');
       done();
     });

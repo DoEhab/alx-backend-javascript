410715e9d30e318c0a8926b71eb0fd2572dac0ec
dohaehab <dohaehab@gmail.com>
2025-06-13 16:13:14 +0300

    task 3

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,5 +1,32 @@
-const request = require('request');
 const chai = require('chai');
 const expect = chai.expect;
 const app = require('./api');
-const http = require('http');
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+
+describe('return http responses', () => {
+    it('return status code', (done) =>{
+        chai.request(app)
+        .get('/')
+        .end((err, res) => {
+            expect(res).to.have.status(200);
+            done();
+        });
+    });
+    it('return data', (done) =>{
+        chai.request(app)
+        .get('/')
+        .end((err, res) => {
+            expect(res.text).to.equal('Welcome to the payment system');
+            done();
+        });
+    });
+    it('return header', (done) =>{
+        chai.request(app)
+        .get('/')
+        .end((err, res) => {
+            expect(res).to.have.header('content-type', /text\/html/);
+            done();
+        });
+    });
+});
